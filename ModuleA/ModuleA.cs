6c16f4c5f5482ae14d5400e6741d8275b901e08b
gmzdev <gmzdev1986@outlook.ph>
2017-06-07 21:37:20 -0700

    Exploring Prism 5.0 - created new ToDoListWPF.Prism project - created Shell for Prism Fx - created UnityBootstrapper - created Initial ModuleA

---
+++ ModuleA/ModuleA.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Practices.Prism.Modularity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModuleA
+{
+    public class ModuleA : IModule
+    {
+        public void Initialize()
+        {
+            //throw new NotImplementedException();
+        }
+    }
+}
---
+++ ToDoListWPF.Prism/Bootstrapper.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Practices.Prism.Modularity;
+using Microsoft.Practices.Prism.UnityExtensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ToDoListWPF.Prism
+{
+    public class Bootstrapper : UnityBootstrapper
+    {
+        protected override DependencyObject CreateShell()
+        {
+            return new Shell();
+        }
+
+        protected override void InitializeShell()
+        {
+            base.InitializeShell();
+
+            Application.Current.MainWindow = (Window)this.Shell;
+            Application.Current.MainWindow.Show();
+        }
+
+        protected override void ConfigureModuleCatalog()
+        {
+            base.ConfigureModuleCatalog();
+
+            ModuleCatalog moduleCatalog = (ModuleCatalog)this.ModuleCatalog;
+            moduleCatalog.AddModule(typeof(ModuleA.ModuleA));
+        }
+    }
+}
